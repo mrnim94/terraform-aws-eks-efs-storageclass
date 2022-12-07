d6eda44cf855a8d311039abcd97e459621a49fb2
mr nim <mrnim@nimtechnology.com>
2022-12-08 00:10:11 +0700

    adjust storage_class

--- c4-02-storage-class.tf
+++ c4-02-storage-class.tf
@@ -1,7 +1,7 @@
 # Resource: Kubernetes Storage Class
 resource "kubernetes_storage_class_v1" "efs_sc" {  
   metadata {
-    name = "efs-sc-$(var.efs_name)"
+    name = "efs-sc-${var.efs_name}"
   }
   storage_provisioner = "efs.csi.aws.com"  
   parameters = {
